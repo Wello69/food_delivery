8c42890a28a814ab52ff9cf8b06601738a6bd8b8
Wello <wello6668@gmail.com>
2025-08-29 02:22:42 +0300

    Feat: add functional SearchBar in HomeScreen

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -14,37 +14,110 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Food Delivery', style: TextStyle(fontSize:20.0, fontWeight:FontWeight.bold, color:Colors.black87)),
+        title: const Text(
+          'Food Delivery.',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
         centerTitle: true,
         backgroundColor: Colors.blue,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print('Moving to cart screen.'); //  Cart Screen
+            },
+            icon: const Icon(Icons.shopping_cart_outlined, color: Colors.white,),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(height: 50, margin: const EdgeInsets.all(16), color: Colors.blue[100]), // SearchBar placeholder
-            Container(height: 150, margin: const EdgeInsets.symmetric(horizontal: 16), color: Colors.blue[200]), // Carousel placeholder
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for food...',
+                  hintStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[300],
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 24.0,
+                    color: Colors.blue[300],
+                  ),
+                  filled: true,
+                  fillColor: Colors.blue[50],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 150,
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              color: Colors.blue[200],
+            ), // Carousel placeholder
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Text('Featured Items', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              child: Text(
+                'Featured Items.',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
             ),
             Row(
               children: [
                 Expanded(
                   child: GestureDetector(
                     onTap: () => setState(() => isGrid = false),
-                    child: Container(height: 40, margin: const EdgeInsets.symmetric(horizontal: 16), color: isGrid ? Colors.blue[200] : Colors.blue, alignment: Alignment.center, child: const Text('List View', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold))),
+                    child: Container(
+                      height: 40,
+                      margin: const EdgeInsets.symmetric(horizontal: 16),
+                      color: isGrid ? Colors.blue[200] : Colors.blue,
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'List View',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
                 Expanded(
                   child: GestureDetector(
                     onTap: () => setState(() => isGrid = true),
-                    child: Container(height: 40, margin: const EdgeInsets.symmetric(horizontal: 16), color: isGrid ? Colors.blue : Colors.blue[200], alignment: Alignment.center, child: const Text('Grid View', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold))),
+                    child: Container(
+                      height: 40,
+                      margin: const EdgeInsets.symmetric(horizontal: 16),
+                      color: isGrid ? Colors.blue : Colors.blue[200],
+                      alignment: Alignment.center,
+                      child: const Text(
+                        'Grid View',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ],
             ),
-            Container(height: 200, margin: const EdgeInsets.all(16), color: Colors.blue[50]), // Products placeholder
+            Container(
+              height: 200,
+              margin: const EdgeInsets.all(16),
+              color: Colors.blue[50],
+            ), // Products placeholder
           ],
         ),
       ),
