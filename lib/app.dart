cba0c2a5ebbfb669bfc10fb9c16c20edaf28be5e
Wello <wello6668@gmail.com>
2025-08-28 11:49:41 +0300

    Feat: add MyApp class with MaterialApp and blue theme

---
+++ lib/app.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'features/home/screens/home_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food Delivery',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,  
+        scaffoldBackgroundColor: Colors.blue[50],
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,     
+          elevation: 2,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue[400], 
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
